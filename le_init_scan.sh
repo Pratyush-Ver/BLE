b32e31e922f62eb52f52f55abb59d1a1dadbab0e
ScratchnestMPU <aditya@scratchnest.com>
2021-07-29 21:56:36 +0530

    Shell script for BT init and scan(Python T_lescan_OK)

---
+++ le_init_scan.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Bluetooth device init over USB-CDC
+
+dev_id=$(ls /dev | grep ttyACM)
+
+if [[ -n $dev_id ]]; then
+  echo "BT module present"
+  d="/dev/"$dev_id
+  btattach -B $d -S 1000000 -P h4 &
+  sleep 3
+  btmgmt --index 0 static-addr FF:00:00:00:00:FF
+  btmgmt --index 0 auto-power
+  python3 lescan.py
+  echo "DONE"
+else
+  echo "No device found"
+fi
